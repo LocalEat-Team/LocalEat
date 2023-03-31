a01568225faed8674aa3e90cfb04cea59461a3e6
laura-nesta <laura.nesta@outlook.com>
2023-03-31 11:05:50 +0200

    style add product

--- src/components/organisms/AddMerchantForm/AddMerchantForm.jsx
+++ src/components/organisms/AddMerchantForm/AddMerchantForm.jsx
@@ -5,85 +5,91 @@ import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 
 export const AddMerchantForm = () => {
+  const dispatch = useDispatch();
+  const shopStatus = useSelector((state) => state.shops.status);
 
-    const dispatch = useDispatch()
-    const shopStatus = useSelector(state => state.shops.status)
-    
-    const [shopName, setShopName] = useState("");
-    const [shopDescription, setShopDescription] = useState("");
-    const [shopAdress, setShopAdress] = useState("");
-    const [x, setX] = useState("");
-    const [y, setY] = useState("");
+  const [shopName, setShopName] = useState("");
+  const [shopDescription, setShopDescription] = useState("");
+  const [shopAdress, setShopAdress] = useState("");
+  const [x, setX] = useState("");
+  const [y, setY] = useState("");
 
-    useEffect(() => {
-        if (shopStatus === 'fulfilled') {
-            
-            alert("Ajout réussi !")
-        }
-        dispatch(shopActions.resetStatus())
-      }, [shopStatus, dispatch])
+  useEffect(() => {
+    if (shopStatus === "fulfilled") {
+      alert("Ajout réussi !");
+    }
+    dispatch(shopActions.resetStatus());
+  }, [shopStatus, dispatch]);
 
-    return(
-        <div style={{textAlign: "center"}}>
-            <h3>Veuillez ajouter une boutique</h3>
-            <form>
-                <TextField 
-                    style={{ width: "350px", margin: "5px" }}
-                    type="text"
-                    label="Nom"
-                    variant="outlined"
-                    value={shopName}
-                    onChange={(e) => setShopName(e.target.value)}
-                />
-                <br/>
-                <TextField
-                    style={{ width: "350px", margin: "5px" }}
-                    type="text"
-                    label="Description"
-                    variant="outlined"
-                    value={shopDescription}
-                    onChange={(e) => setShopDescription(e.target.value)}
-                />
-                <br/>
-                <TextField
-                    style={{ width: "350px", margin: "5px" }}
-                    type="text"
-                    label="Adresse"
-                    variant="outlined"
-                    value={shopAdress}
-                    onChange={(e) => setShopAdress(e.target.value)}
-                />
-                <br/>
-                <TextField
-                    style={{ width: "350px", margin: "5px" }}
-                    type="text"
-                    label="X"
-                    variant="outlined"
-                    value={x}
-                    onChange={(e) => setX(e.target.value)}
-                />
-                <br/>
-                <TextField
-                    style={{ width: "350px", margin: "5px" }}
-                    type="text"
-                    label="Y"
-                    variant="outlined"
-                    value={y}
-                    onChange={(e) => setY(e.target.value)}
-                />
-                <br/>
-                <p>Cette boutique sera enregistré à vos coordonnées GPS.</p>
-                <Button onClick={() => {store.dispatch(addShop({
-                     shopName: shopName,
-                     shopDescription: shopDescription,
-                     shopAdress: shopAdress,
-                     Location_coordinates: [x, y]
-                }
-                ))
-                console.log("Status : "+shopStatus)}} variant="contained" color="primary" style={{ width: "200px", margin: "5px" }}>
-                    Ajouter
-                </Button>
-            </form>
-        </div>
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h3>Veuillez ajouter une boutique</h3>
+      <form>
+        <TextField
+          style={{ width: "350px", margin: "5px" }}
+          type="text"
+          label="Nom"
+          variant="outlined"
+          value={shopName}
+          onChange={(e) => setShopName(e.target.value)}
+        />
+        <br />
+        <TextField
+          style={{ width: "350px", margin: "5px" }}
+          type="text"
+          label="Description"
+          variant="outlined"
+          value={shopDescription}
+          onChange={(e) => setShopDescription(e.target.value)}
+        />
+        <br />
+        <TextField
+          style={{ width: "350px", margin: "5px" }}
+          type="text"
+          label="Adresse"
+          variant="outlined"
+          value={shopAdress}
+          onChange={(e) => setShopAdress(e.target.value)}
+        />
+        <br />
+        <TextField
+          style={{ width: "350px", margin: "5px" }}
+          type="text"
+          label="Coordonnée X (Latitude)"
+          variant="outlined"
+          value={x}
+          onChange={(e) => setX(e.target.value)}
+        />
+        <br />
+        <TextField
+          style={{ width: "350px", margin: "5px" }}
+          type="text"
+          label="Coordonnée Y (Longitude)"
+          variant="outlined"
+          value={y}
+          onChange={(e) => setY(e.target.value)}
+        />
+        <br />
+        <p>Cette boutique sera enregistré à vos coordonnées GPS.</p>
+        <Button
+          onClick={() => {
+            store.dispatch(
+              addShop({
+                shopName: shopName,
+                shopDescription: shopDescription,
+                shopAdress: shopAdress,
+                Location_coordinates: [x, y],
+              })
+            );
+            console.log("Status : " + shopStatus);
+          }}
+          variant="contained"
+          color="primary"
+          style={{ width: "200px", margin: "5px", backgroundColor: "#77CCBB" }}
+        >
+          Ajouter
+        </Button>
+      </form>
+    </div>
   );
 };
